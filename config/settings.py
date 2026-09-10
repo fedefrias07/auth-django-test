f1c667c4b3f8566444238d2fe17fb514118989a0
fedefrias07 <fede.frias07@gmail.com>
2026-09-10 12:05:56 -0300

    chore: :wrench: Agregar variable a ssl

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=True
+        ssl_require= config('DATABASE_SSL_REQUIRE', cast=bool),
     )}
 
 
